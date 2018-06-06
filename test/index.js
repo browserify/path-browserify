ed0793835dcaa046c94ea7c470463df8afce92eb
Renée Kooi <renee@kooi.me>
2018-06-06 12:49:53 +0200

    Port test-path-isabsolute.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ require('./test-path');
 require('./test-path-basename');
 require('./test-path-dirname');
 require('./test-path-extname');
+require('./test-path-isabsolute');
 require('./test-path-join');
 require('./test-path-relative');
 require('./test-path-resolve');
